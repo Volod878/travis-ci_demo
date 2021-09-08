9971dfadeef0ef816b5d7f5bcae70ac4f5037d37
Volod878 <volod1988@gmail.com>
2021-09-09 04:07:02 +0700

    Added class Main and its test

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,26 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    Main main;
+
+    @Before
+    public void load() {
+        main = new Main();
+    }
+
+    @Test
+    public void shouldBigger() {
+        assertTrue(main.isBigger(10, 5));
+        assertFalse(main.isBigger(5, 10));
+        assertFalse(main.isBigger(5, 5));
+    }
+
+    @Test
+    public void main() {
+        Main.main(null);
+    }
+}
